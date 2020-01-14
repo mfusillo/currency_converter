e18c211528d98e5de45bd4565b77761dda2e1c35
Matteo Fusillo <matt.fusillo@gmail.com>
2020-01-14 17:59:17 +0000

    add convertfromeuro function

--- src/app.js
+++ src/app.js
@@ -4,10 +4,31 @@ document.addEventListener('DOMContentLoaded', () => {
   new Vue({
     el: "#app",
     data: {
-
+      currencyRates: null,
+      userAmount: null,
+      userAmountToEuro:null,
+      currencySelected: null,
+      convertedValueFromEuro: null,
+      convertedValueToEuro: null
     },
-    methods: {
 
+    mounted(){
+      this.fetchCurrency()
+    },
+
+    methods: {
+      fetchCurrency: function(){
+        fetch('https://api.exchangeratesapi.io/latest')
+        .then(response => response.json())
+        .then(result => this.currencyRates = result.rates)
+      },
+      convertFromEuro: function(){
+        this.convertedValueFromEuro = (this.userAmount * this.currencySelected.value).toFixed(2)
+      },
+
+      convertToEuro: function(){
+        // this.convertedValueToEuro = (this.userAmountToEuro * )
+      }
     }
   });
 });
